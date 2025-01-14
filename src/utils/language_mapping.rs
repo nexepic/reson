e15791df9bfff3c1c43c82bbd11d81038e6c6f28
nexepic <remarkable.yh@gmail.com>
2025-01-14 14:02:14 +0800

    header file including

--- src/utils/language_mapping.rs
+++ src/utils/language_mapping.rs
@@ -2,8 +2,8 @@ use std::collections::HashMap;
 
 pub fn get_language_mapping() -> HashMap<&'static str, Vec<&'static str>> {
     let mut mapping = HashMap::new();
-    mapping.insert("c", vec!["c"]);
-    mapping.insert("cpp", vec!["cpp", "cc", "cxx"]);
+    mapping.insert("c", vec!["c", "h"]);
+    mapping.insert("cpp", vec!["cpp", "cc", "cxx", "hpp", "hxx"]);
     mapping.insert("java", vec!["java"]);
     mapping.insert("javascript", vec!["js"]);
     mapping.insert("python", vec!["py"]);
@@ -29,8 +29,8 @@ mod tests {
     #[test]
     fn test_get_language_mapping() {
         let mapping = get_language_mapping();
-        assert_eq!(mapping.get("c"), Some(&vec!["c"]));
-        assert_eq!(mapping.get("cpp"), Some(&vec!["cpp", "cc", "cxx"]));
+        assert_eq!(mapping.get("c"), Some(&vec!["c", "h"]));
+        assert_eq!(mapping.get("cpp"), Some(&vec!["cpp", "cc", "cxx", "hpp", "hxx"]));
         assert_eq!(mapping.get("java"), Some(&vec!["java"]));
         assert_eq!(mapping.get("javascript"), Some(&vec!["js"]));
         assert_eq!(mapping.get("python"), Some(&vec!["py"]));
@@ -41,9 +41,12 @@ mod tests {
     #[test]
     fn test_get_language_from_extension() {
         assert_eq!(get_language_from_extension("c"), Some("c"));
+        assert_eq!(get_language_from_extension("h"), Some("c"));
         assert_eq!(get_language_from_extension("cpp"), Some("cpp"));
         assert_eq!(get_language_from_extension("cc"), Some("cpp"));
         assert_eq!(get_language_from_extension("cxx"), Some("cpp"));
+        assert_eq!(get_language_from_extension("hpp"), Some("cpp"));
+        assert_eq!(get_language_from_extension("hxx"), Some("cpp"));
         assert_eq!(get_language_from_extension("java"), Some("java"));
         assert_eq!(get_language_from_extension("js"), Some("javascript"));
         assert_eq!(get_language_from_extension("py"), Some("python"));
