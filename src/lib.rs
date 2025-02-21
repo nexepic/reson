25c5d15b987a38100140c947af4f1fb4cdd80a5c
nexepic <remarkable.yh@gmail.com>
2025-02-21 16:42:34 +0800

    stack size specification

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub static LARGE_CONTENT_LENGTH_THRESHOLD: usize = 10000;
 pub static LARGE_ARRAY_THRESHOLD: usize = 1000;
 pub static TREE_PARSING_MAX_DEPTH: usize = 1000;
+pub static POOL_STACK_SIZE: usize = 100 * 1024 * 1024; // 100MB
