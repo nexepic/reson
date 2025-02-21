2e21e425c87ec571494a7d06cc76542e308c379f
nexepic <remarkable.yh@gmail.com>
2025-02-21 16:31:11 +0800

    performance optimized, unnecessary records removed

--- src/models/detection_types.rs
+++ src/models/detection_types.rs
@@ -1,5 +1,4 @@
 use serde::Serialize;
-use std::collections::{BTreeSet, HashMap};
 
 #[derive(Serialize, Debug, Clone)]
 pub struct DuplicateBlock {
@@ -23,12 +22,5 @@ pub struct DuplicateReportXML<T> {
 #[derive(Serialize, Clone)]
 pub struct ParentFingerprint {
     pub fingerprint: String,
-    pub content: String,
-}
-
-#[derive(Serialize)]
-pub struct DebugData {
-    pub parent_fingerprints: HashMap<String, ParentFingerprint>,
-    pub exceeding_threshold_fingerprints: BTreeSet<String>,
-    // pub content_fingerprint_mappings: Vec<(String, usize, usize, String, String, String)>, // (content, start_line, end_line, fingerprint, file_name, ast_content)
+    // pub content: String,
 }
