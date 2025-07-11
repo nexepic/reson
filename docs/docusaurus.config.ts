7618e1e90cdf197fd66b9fa5f165d8230c7abe6f
nexepic <remarkable.yh@gmail.com>
2025-07-11 13:30:42 +0800

    site fix

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   // },
 
   // Set the production url of your site here
-  url: 'https://nexepic.github.io/reson',
+  url: 'https://nexepic.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/reson/',
